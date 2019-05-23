fbf59340cd7b2807985f1da1890c9ead5357f9a1
Lisa Harrylock <lisah@alleninstitute.org>
2019-05-23 16:32:19 -0700

    add query endpoint

--- src/renderer/util/labkey-client/index.ts
+++ src/renderer/util/labkey-client/index.ts
@@ -1,15 +1,23 @@
 import axios from "axios";
 
-import { LABKEY_SELECT_ROWS_URL } from "../../constants";
+import { LABKEY_SELECT_ROWS_URL, LK_MICROSCOPY_SCHEMA } from "../../constants";
+import { HttpClient } from "../../state/types";
 
 export interface Plate {
     BarCode: string;
+    ImagingSessionId: string;
 }
 interface GetBarcodesResponse {
     data: {
         rowCount: number,
         rows: Plate[],
     };
+}
+
+export interface ImagingSession {
+    ImagingSessionId: number;
+    Name: string;
+    Description: string;
 }
 
 class Get {
@@ -22,6 +30,12 @@ class Get {
             .then((response: GetBarcodesResponse) => {
                 return response.data.rows;
             });
+    }
+
+    public static async imagingSessions(httpClient: HttpClient): Promise<ImagingSession[]> {
+        const query = LABKEY_SELECT_ROWS_URL(LK_MICROSCOPY_SCHEMA, "ImagingSession");
+        const response = await httpClient.get(query);
+        return response.data.rows;
     }
 }
 
