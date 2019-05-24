2b647bd9b57c756578bf94e31e3b6454c59a2e2a
Lisa Harrylock <lisah@alleninstitute.org>
2019-05-24 13:43:46 -0700

    fix lint

--- src/renderer/state/upload/logics.ts
+++ src/renderer/state/upload/logics.ts
@@ -17,7 +17,12 @@ import { addJob, setCurrentJobId, setJobs, setUploadStatus } from "../job/action
 import { getCurrentJob, getCurrentJobIndex, getJobs } from "../job/selectors";
 import { deselectFiles } from "../selection/actions";
 import { getSelectedBarcode, getWell } from "../selection/selectors";
-import { ReduxLogicDoneCb, ReduxLogicNextCb, ReduxLogicProcessDependencies, ReduxLogicTransformDependencies } from "../types";
+import {
+    ReduxLogicDoneCb,
+    ReduxLogicNextCb,
+    ReduxLogicProcessDependencies,
+    ReduxLogicTransformDependencies,
+} from "../types";
 import { batchActions } from "../util";
 import { ASSOCIATE_FILES_AND_WELL, INITIATE_UPLOAD } from "./constants";
 import { getUploadPayload } from "./selectors";
