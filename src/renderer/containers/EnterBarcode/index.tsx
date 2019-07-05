67ba0751df0dfe96136933bcda147bcddd4e393a
seanm <seanm@alleninstitute.org>
2019-07-05 16:30:53 -0700

    Autofocus barcode input

--- src/renderer/containers/EnterBarcode/index.tsx
+++ src/renderer/containers/EnterBarcode/index.tsx
@@ -128,6 +128,7 @@ class EnterBarcode extends React.Component<EnterBarcodeProps, EnterBarcodeState>
                 onBack={this.props.goBack}
             >
                 <LabKeyOptionSelector
+                    autofocus={true}
                     required={true}
                     async={true}
                     label="Plate Barcode"
