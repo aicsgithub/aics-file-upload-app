8d7a879ed78faae0787b8ac360c078e3aa9abda2
Lisa Harrylock <lisah@alleninstitute.org>
2019-07-18 14:17:53 -0700

    show metadata dynamically

--- src/renderer/components/UploadJobDisplay/index.tsx
+++ src/renderer/components/UploadJobDisplay/index.tsx
@@ -1,59 +1,41 @@
 import { UploadMetadata } from "@aics/aicsfiles/type-declarations/types";
 import { Collapse, Descriptions } from "antd";
 import CollapsePanel from "antd/lib/collapse/CollapsePanel";
+import { decamelizeKeys} from "humps";
 import { isEmpty, map } from "lodash";
 import * as React from "react";
 import { UploadSummaryTableRow } from "../../containers/UploadSummary";
 
 const Item = Descriptions.Item;
-// const styles = require("./styles.pcss");
+const SEPARATOR = { separator: " "};
 
 interface MetadataDisplayProps {
     title: string;
     metadata: any;
 }
-const MetadataDisplay: React.FunctionComponent<MetadataDisplayProps> = ({metadata, title}: MetadataDisplayProps) => (
-    <Descriptions
-        title={title}
-        column={{xs: 1}}
-        bordered={true}
-    >
-        {map(metadata, (value: any, key: string) => {
-            if (typeof value === "object") {
-                if (Array.isArray(value)) {
-                    return <Item label={key}>{value.join(", ")}</Item>;
+const MetadataDisplay: React.FunctionComponent<MetadataDisplayProps> = ({metadata, title}: MetadataDisplayProps) => {
+    metadata = decamelizeKeys(metadata, SEPARATOR);
+    title = `${title.charAt(0).toUpperCase()}${title.slice(1)} Metadata`;
+    return (
+        <Descriptions
+            size="small"
+            title={title}
+            column={{xs: 1}}
+            bordered={false}
+        >
+            {map(metadata, (value: any, key: string) => {
+                if (typeof value === "object") {
+                    if (Array.isArray(value)) {
+                        return <Item label={key} key={key}>{value.join(", ")}</Item>;
+                    }
+                    return <MetadataDisplay title={key} metadata={value} key={key}/>;
                 }
-                return <MetadataDisplay title={key} metadata={value}/>;
-            }
 
-            return <Item label={key}>{value}</Item>;
-        })}
-    </Descriptions>
-);
-
-interface FileDisplayProps {
-    key: string;
-    metadata: UploadMetadata;
-
-    //     {
-    //     file: {
-    //         fileType: string;
-    //         originalPath: string;
-    //     },
-    //     microscopy: {
-    //         wellIds: number[];
-    //     }
-    // };
-}
-
-const FileDisplay: React.FunctionComponent<FileDisplayProps> = ({ key, metadata }: FileDisplayProps) => (
-    <CollapsePanel header={metadata.file.originalPath} key={key}>
-        <h2>{metadata.file.originalPath}</h2>
-        {map(metadata, (value: any, metadataGroupName: string) => (
-            <MetadataDisplay metadata={value} title={metadataGroupName} key={metadataGroupName}/>
-        ))}
-    </CollapsePanel>
-);
+                return <Item label={key} key={key}>{value}</Item>;
+            })}
+        </Descriptions>
+    );
+};
 
 interface UploadJobDisplayProps {
     className?: string;
@@ -73,12 +55,15 @@ const UploadJobDisplay: React.FunctionComponent<UploadJobDisplayProps> = ({class
         status,
         user,
     } = job;
-    const showFiles = serviceFields && serviceFields.files && Array.isArray(serviceFields.files) && !isEmpty(serviceFields.files);
+    const showFiles = serviceFields && serviceFields.files && Array.isArray(serviceFields.files)
+        && !isEmpty(serviceFields.files);
+
     return (
         <div className={className}>
             <Descriptions
+                size="small"
                 title="Job Overview"
-                bordered={true}
+                bordered={false}
                 column={{ xxl: 3, xl: 2, lg: 2, md: 2, sm: 1, xs: 1 }}
             >
                 <Item label="Job Id">{jobId}</Item>
@@ -91,18 +76,31 @@ const UploadJobDisplay: React.FunctionComponent<UploadJobDisplayProps> = ({class
                 <Item label="Modified">{modified.toLocaleString()}</Item>
                 <Item label="Current Stage">{currentStage}</Item>
             </Descriptions>
-            {showFiles && (
-                <Collapse>
-                    {serviceFields.files.map((f: UploadMetadata, i: number) => {
-                        if (!f.file.originalPath) {
-                            return null;
-                        }
 
-                        return (
-                            <FileDisplay key={f.file.originalPath} metadata={f}/>
-                        );
-                    })}
-                </Collapse>
+            {showFiles && (
+                <>
+                    <div className="ant-descriptions-title">Files</div>
+                    <Collapse>
+                        {serviceFields.files.map((f: UploadMetadata) => {
+                            if (!f.file.originalPath) {
+                                return null;
+                            }
+
+                            return (
+                                <CollapsePanel header={f.file.originalPath} key={f.file.originalPath}>
+
+                                    {map(f, (value: any, metadataGroupName: string) => (
+                                        <MetadataDisplay
+                                            metadata={value}
+                                            title={metadataGroupName}
+                                            key={metadataGroupName}
+                                        />
+                                    ))}
+                                </CollapsePanel>
+                            );
+                        })}
+                    </Collapse>
+                </>
             )}
         </div>
     );
