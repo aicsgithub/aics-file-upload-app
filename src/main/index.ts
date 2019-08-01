0f85e3f12857c87f96ca14eb67971a82193c3800
seanm <seanm@alleninstitute.org>
2019-08-01 12:20:29 -0700

    Remove sneaky quote

--- src/main/index.ts
+++ src/main/index.ts
@@ -124,7 +124,7 @@ ipcMain.on(OPEN_CREATE_PLATE_STANDALONE, (event: any, barcode: string, prefix: s
     const plateView = `/labkey/aics_microscopy/AICS/plateStandalone.view?Barcode=${barcode}`;
     let modalUrl = `${LIMS_PROTOCOL}://${LIMS_HOST}:${LIMS_PORT}${plateView}`;
     if (prefix === "AX" || prefix === "AD") {
-        modalUrl = `${modalUrl}&TeamMode=AssayDev"`;
+        modalUrl = `${modalUrl}&TeamMode=AssayDev`;
     }
     child.loadURL(modalUrl);
     child.once("ready-to-show", () => {
