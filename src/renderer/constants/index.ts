3c2bb4db0c6a10e8fdb45813d3ac3366f4a6be55
seanm <seanm@alleninstitute.org>
2019-08-16 14:05:31 -0700

    Add schemas and LK get tables url

--- src/renderer/constants/index.ts
+++ src/renderer/constants/index.ts
@@ -15,8 +15,18 @@ export const LABKEY_SELECT_ROWS_URL = (schema: string, table: string, additional
     return base;
 };
 
+export const LABKEY_GET_TABLES_URL = (): string => `${LABKEY_URL}/AICS/query-getQueries.api`;
+
 // Metadata Management Service
 export const MMS_BASE_URL = `${LIMS_PROTOCOL}://${HOST}/metadata-management-service`;
 
 // Labkey Schemas
 export const LK_MICROSCOPY_SCHEMA = "microscopy";
+
+// There are more schemas, but these are the only ones (AFAIK) that users use
+export const SCHEMAS = [
+    "assayscustom",
+    "celllines",
+    LK_MICROSCOPY_SCHEMA,
+    "processing"
+];
