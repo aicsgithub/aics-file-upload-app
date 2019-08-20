2fcb291362267f1b1ffc53f414feca3407b724c8
seanm <seanm@alleninstitute.org>
2019-08-20 12:19:36 -0700

    Change UI depending on if upload is by workflow or plate

---
+++ src/renderer/components/AssociateWorkflows/index.tsx
@@ -0,0 +1,112 @@
+import { LabKeyOptionSelector } from "@aics/aics-react-labkey";
+import { keys } from "lodash";
+import * as React from "react";
+import { ActionCreator } from "redux";
+
+import { GoBackAction, NextPageAction, SelectWorkflowsAction, Workflow } from "../../state/selection/types";
+import {
+    AssociateFilesAndWorkflowsAction,
+    UndoFileWorkflowAssociationAction
+} from "../../state/upload/types";
+import { IdToFilesMap } from "../../containers/AssociateFiles/selectors";
+import { RequestWorkflowOptionsAction } from "../../state/metadata/types";
+import FormPage from "../FormPage";
+import SelectedAssociationsCard from "../SelectedAssociationsCard";
+
+const styles = require("./style.pcss");
+
+interface Props {
+    associateFilesAndWorkflows: ActionCreator<AssociateFilesAndWorkflowsAction>;
+    canRedo: boolean;
+    canUndo: boolean;
+    className?: string;
+    loadWorkflows: ActionCreator<RequestWorkflowOptionsAction>;
+    goBack: ActionCreator<GoBackAction>;
+    goForward: ActionCreator<NextPageAction>;
+    mutualFiles: string[];
+    redo: () => void;
+    selectedFiles: string[];
+    selectedWorkflows: Workflow[];
+    selectWorkflows: ActionCreator<SelectWorkflowsAction>;
+    undo: () => void;
+    undoAssociation: ActionCreator<UndoFileWorkflowAssociationAction>;
+    workflowOptions: Workflow[];
+    workflowIdToFiles: IdToFilesMap;
+}
+
+class AssociateWorkflows extends React.Component<Props, {}> {
+    componentDidMount() {
+        this.props.loadWorkflows();
+    }
+
+    public render() {
+        const {
+            canRedo,
+            canUndo,
+            className,
+            mutualFiles,
+            redo,
+            selectedFiles,
+            selectedWorkflows,
+            selectWorkflows,
+            undo,
+            workflowOptions,
+        } = this.props;
+
+        return (
+            <FormPage
+                className={className}
+                formTitle="ASSOCIATE WORKFLOWS"
+                formPrompt="Associate files and workflows by selecting them and clicking Associate"
+                onBack={this.props.goBack}
+                onSave={this.props.goForward}
+                saveButtonDisabled={!this.canContinue()}
+            >
+                <SelectedAssociationsCard
+                    selectedWorkflows={selectedWorkflows}
+                    files={mutualFiles}
+                    selectedFilesCount={selectedFiles.length}
+                    associate={this.associate}
+                    canAssociate={this.canAssociate()}
+                    undoAssociation={this.undoAssociation}
+                    undoLastAssociation={undo}
+                    redo={redo}
+                    canRedo={canRedo}
+                    canUndoLastAssociation={canUndo}
+                />
+                    <div className={styles.workflowSelector}>
+                        <LabKeyOptionSelector
+                            autoFocus={true}
+                            label="Workflow"
+                            multiSelect={true}
+                            onOptionSelection={selectWorkflows}
+                            optionIdKey="workflowId"
+                            optionNameKey="name"
+                            options={workflowOptions}
+                            placeholder="Select Workflow(s)"
+                            required={true}
+                            selected={selectedWorkflows}
+                        />
+                    </div>
+            </FormPage>
+        );
+    }
+
+    private undoAssociation = (file: string): void => {
+        this.props.undoAssociation(file, this.props.selectedWorkflows);
+    }
+
+    private canAssociate = (): boolean => {
+        return this.props.selectedWorkflows.length > 0 && this.props.selectedFiles.length > 0;
+    }
+
+    private associate = (): void => {
+        this.props.associateFilesAndWorkflows(this.props.selectedFiles, this.props.selectedWorkflows);
+    }
+
+    private canContinue = (): boolean => {
+        return keys(this.props.workflowIdToFiles).length > 0;
+    }
+}
+
+export default AssociateWorkflows;
---
+++ src/renderer/containers/AssociateFiles/index.tsx
@@ -0,0 +1,176 @@
+import { AicsGridCell } from "@aics/aics-react-labkey";
+import * as React from "react";
+import { connect } from "react-redux";
+import { ActionCreator } from "redux";
+
+import {
+    goBack,
+    goForward,
+    selectWells,
+    selectWorkflows
+} from "../../state/selection/actions";
+import {
+    getSelectedFiles,
+    getSelectedWellLabels,
+    getSelectedWells,
+    getSelectedWellsWithData,
+    getSelectedWorkflows,
+    getWellsWithUnitsAndModified,
+} from "../../state/selection/selectors";
+import {
+    GoBackAction,
+    NextPageAction,
+    SelectWellsAction,
+    SelectWorkflowsAction,
+    Well,
+    Workflow,
+} from "../../state/selection/types";
+import { State } from "../../state/types";
+import {
+    associateFilesAndWells,
+    associateFilesAndWorkflows,
+    jumpToUpload,
+    undoFileWellAssociation,
+    undoFileWorkflowAssociation
+} from "../../state/upload/actions";
+import { getCanRedoUpload, getCanUndoUpload } from "../../state/upload/selectors";
+import {
+    AssociateFilesAndWellsAction,
+    AssociateFilesAndWorkflowsAction,
+    JumpToUploadAction,
+    UndoFileWellAssociationAction,
+    UndoFileWorkflowAssociationAction,
+} from "../../state/upload/types";
+import AssociateWells from "../../components/AssociateWells";
+import AssociateWorkflows from "../../components/AssociateWorkflows";
+import { getAssociateByWorkflow } from "../../state/setting/selectors";
+import {
+    getMutualFilesForWells,
+    getMutualFilesForWorkflows,
+    getWellIdToFiles,
+    getWorkflowIdToFiles,
+    IdToFilesMap
+} from "./selectors";
+import { RequestWorkflowOptionsAction } from "../../state/metadata/types";
+import { getWorkflowOptions } from "../../state/metadata/selectors";
+import { requestWorkflows } from "../../state/metadata/actions";
+
+interface AssociateFilesProps {
+    associateWorkflows: boolean;
+    associateFilesAndWells: ActionCreator<AssociateFilesAndWellsAction>;
+    associateFilesAndWorkflows: ActionCreator<AssociateFilesAndWorkflowsAction>;
+    canRedo: boolean;
+    canUndo: boolean;
+    className?: string;
+    mutualFilesForWells: string[];
+    mutualFilesForWorkflows: string[];
+    goBack: ActionCreator<GoBackAction>;
+    goForward: ActionCreator<NextPageAction>;
+    requestWorkflows: ActionCreator<RequestWorkflowOptionsAction>;
+    selectedFiles: string[];
+    selectedWellLabels: string[];
+    selectedWells: AicsGridCell[];
+    selectedWellsData: Well[];
+    selectedWorkflows: Workflow[];
+    selectWells: ActionCreator<SelectWellsAction>;
+    selectWorkflows: ActionCreator<SelectWorkflowsAction>;
+    wells?: Well[][];
+    wellIdToFiles: IdToFilesMap;
+    workflowIdToFiles: IdToFilesMap;
+    workflowOptions: Workflow[];
+    jumpToUpload: ActionCreator<JumpToUploadAction>;
+    undoFileWellAssociation: ActionCreator<UndoFileWellAssociationAction>;
+    undoFileWorkflowAssociation: ActionCreator<UndoFileWorkflowAssociationAction>;
+}
+
+class AssociateFiles extends React.Component<AssociateFilesProps, {}> {
+    public render() {
+        if (this.props.associateWorkflows) {
+            return (
+                <AssociateWorkflows
+                    associateFilesAndWorkflows={this.props.associateFilesAndWorkflows}
+                    canRedo={this.props.canRedo}
+                    canUndo={this.props.canUndo}
+                    className={this.props.className}
+                    mutualFiles={this.props.mutualFilesForWorkflows}
+                    loadWorkflows={this.props.requestWorkflows}
+                    goBack={this.props.goBack}
+                    goForward={this.props.goForward}
+                    redo={this.redo}
+                    selectWorkflows={this.props.selectWorkflows}
+                    selectedFiles={this.props.selectedFiles}
+                    selectedWorkflows={this.props.selectedWorkflows}
+                    workflowIdToFiles={this.props.workflowIdToFiles}
+                    undo={this.undo}
+                    undoAssociation={this.props.undoFileWorkflowAssociation}
+                    workflowOptions={this.props.workflowOptions}
+                />
+            )
+        }
+        return (
+            <AssociateWells
+                associateFilesAndWells={this.props.associateFilesAndWells}
+                canRedo={this.props.canRedo}
+                canUndo={this.props.canUndo}
+                className={this.props.className}
+                mutualFilesForWells={this.props.mutualFilesForWells}
+                goBack={this.props.goBack}
+                goForward={this.props.goForward}
+                redo={this.redo}
+                selectedFiles={this.props.selectedFiles}
+                selectWells={this.props.selectWells}
+                selectedWells={this.props.selectedWells}
+                selectedWellLabels={this.props.selectedWellLabels}
+                selectedWellsData={this.props.selectedWellsData}
+                wellIdToFiles={this.props.wellIdToFiles}
+                wells={this.props.wells}
+                undo={this.undo}
+                undoAssociation={this.props.undoFileWellAssociation}
+            />
+        );
+    }
+
+    private undo = (): void => {
+        this.props.jumpToUpload(-1);
+    }
+
+    private redo = (): void => {
+        this.props.jumpToUpload(1);
+    }
+}
+
+function mapStateToProps(state: State) {
+    return {
+        associateWorkflows: getAssociateByWorkflow(state),
+        canRedo: getCanRedoUpload(state),
+        canUndo: getCanUndoUpload(state),
+        mutualFilesForWells: getMutualFilesForWells(state),
+        mutualFilesForWorkflows: getMutualFilesForWorkflows(state),
+        selectedFiles: getSelectedFiles(state),
+        selectedWellLabels: getSelectedWellLabels(state),
+        selectedWells: getSelectedWells(state),
+        selectedWellsData: getSelectedWellsWithData(state),
+        selectedWorkflows: getSelectedWorkflows(state),
+        wellIdToFiles: getWellIdToFiles(state),
+        workflowIdToFiles: getWorkflowIdToFiles(state),
+        wells: getWellsWithUnitsAndModified(state),
+        workflowOptions: getWorkflowOptions(state),
+    };
+}
+
+const dispatchToPropsMap = {
+    associateFilesAndWells,
+    associateFilesAndWorkflows,
+    getWorkflowOptions,
+    goBack,
+    goForward,
+    jumpToUpload,
+    requestWorkflows,
+    selectWells,
+    selectWorkflows,
+    undoFileWellAssociation,
+    undoFileWorkflowAssociation,
+
+};
+
+export default connect(mapStateToProps, dispatchToPropsMap)(AssociateFiles);
