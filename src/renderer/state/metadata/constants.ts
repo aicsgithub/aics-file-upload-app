c0d8df3894c8d0dee8f3d9272e4dedc61d672399
Lisa Harrylock <lisah@alleninstitute.org>
2019-08-30 14:44:46 -0700

    fix lint error

--- src/renderer/state/metadata/constants.ts
+++ src/renderer/state/metadata/constants.ts
@@ -6,4 +6,4 @@ export const CREATE_BARCODE = makeConstant(BRANCH_NAME, "create-barcode");
 export const RECEIVE_METADATA = makeConstant(BRANCH_NAME, "receive");
 export const REQUEST_METADATA = makeConstant(BRANCH_NAME, "request");
 export const UPDATE_PAGE_HISTORY = makeConstant(BRANCH_NAME, "update-page-history");
-export const GET_BARCODE_SEARCH_RESULTS= makeConstant(BRANCH_NAME, "get-barcode-search-results");
+export const GET_BARCODE_SEARCH_RESULTS = makeConstant(BRANCH_NAME, "get-barcode-search-results");
