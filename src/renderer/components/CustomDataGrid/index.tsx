ccdb54b13dc1a77c1aec36c898f89aa9ff6013ae
Lisa Harrylock <lisah@alleninstitute.org>
2019-11-13 16:11:21 -0800

    fix lint

--- src/renderer/components/CustomDataGrid/index.tsx
+++ src/renderer/components/CustomDataGrid/index.tsx
@@ -124,7 +124,8 @@ class CustomDataGrid extends React.Component<Props, CustomDataState> {
 
     private readonly WORKFLOW_UPLOAD_COLUMNS: UploadJobColumn[] = [
         {
-            formatter: ({ row, value }: FormatterProps<UploadJobTableRow>) => this.renderFormat(row, "workflows", value),
+            formatter: ({ row, value }: FormatterProps<UploadJobTableRow>) =>
+                this.renderFormat(row, "workflows", value),
             key: "workflows",
             name: "Workflows",
             resizable: true,
