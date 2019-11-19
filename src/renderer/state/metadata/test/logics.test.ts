335bd7dbdbf559ea5708ce6a97183ad01839a629
Lisa Harrylock <lisah@alleninstitute.org>
2019-11-18 16:13:51 -0800

    update metadata and template tests

--- src/renderer/state/metadata/test/logics.test.ts
+++ src/renderer/state/metadata/test/logics.test.ts
@@ -42,7 +42,7 @@ describe("Metadata logics", () => {
     });
 
     describe("requestMetadata", () => {
-        it("sets metadata given OK response", (done) => {
+        it("sets metadata given OK response", async () => {
             const getAnnotationLookupsStub = stub().resolves(mockAnnotationLookups);
             const getAnnotationTypesStub = stub().resolves(mockAnnotationTypes);
             const getBarcodePrefixesStub = stub().resolves(mockBarcodePrefixes);
@@ -61,7 +61,7 @@ describe("Metadata logics", () => {
             sandbox.replace(labkeyClient, "getUnits", getUnitsStub);
             sandbox.replace(labkeyClient, "getWorkflows", getWorkflowsStub);
 
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             let state = store.getState();
             expect(getAnnotationLookups(state)).to.be.empty;
@@ -75,24 +75,21 @@ describe("Metadata logics", () => {
 
             store.dispatch(requestMetadata());
 
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            store.subscribe(() => {
-                state = store.getState();
-                expect(getAnnotationLookups(state)).to.not.be.empty;
-                expect(getAnnotationTypes(state)).to.not.be.empty;
-                expect(getBarcodePrefixes(state)).to.not.be.empty;
-                expect(getChannels(state)).to.not.be.empty;
-                expect(getImagingSessions(state)).to.not.be.empty;
-                expect(getLookups(state)).to.not.be.empty;
-                expect(getUnits(state)).to.not.be.empty;
-                expect(getWorkflowOptions(state)).to.not.be.empty;
-                done();
-            });
+            await logicMiddleware.whenComplete();
+            state = store.getState();
+            expect(getAnnotationLookups(state)).to.not.be.empty;
+            expect(getAnnotationTypes(state)).to.not.be.empty;
+            expect(getBarcodePrefixes(state)).to.not.be.empty;
+            expect(getChannels(state)).to.not.be.empty;
+            expect(getImagingSessions(state)).to.not.be.empty;
+            expect(getLookups(state)).to.not.be.empty;
+            expect(getUnits(state)).to.not.be.empty;
+            expect(getWorkflowOptions(state)).to.not.be.empty;
         });
-        it("sets alert given non-OK response", (done) => {
+        it("sets alert given non-OK response", async () => {
             const getImagingSessionsStub = stub().rejects();
             sandbox.replace(labkeyClient, "getImagingSessions", getImagingSessionsStub);
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             // before
             expect(getAlert(store.getState())).to.be.undefined;
@@ -101,23 +98,20 @@ describe("Metadata logics", () => {
             store.dispatch(requestMetadata());
 
             // after
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            store.subscribe(() => {
-                const alert = getAlert(store.getState());
-                expect(alert).to.not.be.undefined;
-                expect(get(alert, "type")).to.equal(AlertType.ERROR);
-                expect(get(alert, "message")).to.contain("Failed to retrieve metadata.");
-                done();
-            });
+            await logicMiddleware.whenComplete();
+            const alert = getAlert(store.getState());
+            expect(alert).to.not.be.undefined;
+            expect(get(alert, "type")).to.equal(AlertType.ERROR);
+            expect(get(alert, "message")).to.contain("Failed to retrieve metadata.");
         });
     });
     describe("requestAnnotations", () => {
-        it("sets annotations given OK response", (done) => {
+        it("sets annotations given OK response", async () => {
             const getAnnotationsStub = stub().resolves(mockAnnotations);
             const getAnnotationOptionsStub = stub().resolves(mockAnnotationOptions);
             sandbox.replace(labkeyClient, "getAnnotations", getAnnotationsStub);
             sandbox.replace(labkeyClient, "getAnnotationOptions", getAnnotationOptionsStub);
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             let state = store.getState();
             expect(getAnnotations(state)).to.be.empty;
@@ -125,82 +119,54 @@ describe("Metadata logics", () => {
 
             store.dispatch(requestAnnotations());
 
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            let count = 0;
-            store.subscribe(() => {
-                count++;
-                if (count > 1) {
-                    state = store.getState();
-                    expect(getAnnotations(state)).to.not.be.empty;
-                    expect(getAnnotationOptions(state)).to.not.be.empty;
-                    done();
-                }
-            });
+            await logicMiddleware.whenComplete();
+            state = store.getState();
+            expect(getAnnotations(state)).to.not.be.empty;
+            expect(getAnnotationOptions(state)).to.not.be.empty;
         });
-        it("sets alert given not OK response", (done) => {
+        it("sets alert given not OK response", async () => {
             const getAnnotationsStub = stub().rejects();
             sandbox.replace(labkeyClient, "getAnnotations", getAnnotationsStub);
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             let state = store.getState();
             expect(getAlert(state)).to.be.undefined;
 
             store.dispatch(requestAnnotations());
 
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            let count = 0;
-            store.subscribe(() => {
-                count++;
-                if (count > 1) {
-                    state = store.getState();
-                    expect(getAlert(state)).to.not.be.undefined;
-                    done();
-                }
-            });
+            await logicMiddleware.whenComplete();
+            state = store.getState();
+            expect(getAlert(state)).to.not.be.undefined;
         });
     });
     describe("requestTemplates", () => {
-        it("sets templates given OK response", (done) => {
+        it("sets templates given OK response", async () => {
             const getTemplatesStub = stub().resolves(mockAnnotations);
             sandbox.replace(labkeyClient, "getTemplates", getTemplatesStub);
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             let state = store.getState();
             expect(getAnnotations(state)).to.be.empty;
 
             store.dispatch(requestTemplates());
 
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            let count = 0;
-            store.subscribe(() => {
-                count++;
-                if (count > 1) {
-                    state = store.getState();
-                    expect(getTemplates(state)).to.not.be.empty;
-                    done();
-                }
-            });
+            await logicMiddleware.whenComplete();
+            state = store.getState();
+            expect(getTemplates(state)).to.not.be.empty;
         });
-        it("sets templates given not OK response", (done) => {
+        it("sets templates given not OK response", async () => {
             const getTemplatesStub = stub().rejects();
             sandbox.replace(labkeyClient, "getTemplates", getTemplatesStub);
-            const { store } = createMockReduxStore(mockState, mockReduxLogicDeps);
+            const { logicMiddleware, store } = createMockReduxStore(mockState, mockReduxLogicDeps);
 
             let state = store.getState();
             expect(getAlert(state)).to.be.undefined;
 
             store.dispatch(requestTemplates());
 
-            // todo FMS-669 stop using subscribe to test multi-dispatch logics
-            let count = 0;
-            store.subscribe(() => {
-                count++;
-                if (count > 1) {
-                    state = store.getState();
-                    expect(getAlert(state)).to.not.be.undefined;
-                    done();
-                }
-            });
+            await logicMiddleware.whenComplete();
+            state = store.getState();
+            expect(getAlert(state)).to.not.be.undefined;
         });
     });
 });
