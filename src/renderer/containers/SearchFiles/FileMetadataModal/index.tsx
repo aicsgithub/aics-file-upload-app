45215a332d520067585d9523ff0d6e73fe9c1b5e
seanm <seanm@alleninstitute.org>
2019-12-06 15:48:23 -0800

    Allow users to browse to files

--- src/renderer/containers/SearchFiles/FileMetadataModal/index.tsx
+++ src/renderer/containers/SearchFiles/FileMetadataModal/index.tsx
@@ -1,5 +1,7 @@
-import { Descriptions, Modal } from "antd";
-import { startCase, forOwn } from "lodash";
+import { Alert, Button, Divider, List, Modal, Row } from "antd";
+import { shell } from "electron";
+import { startCase, forOwn, isNil } from "lodash";
+import os from 'os';
 import * as React from "react";
 
 import { MAIN_FILE_COLUMNS, UNIMPORTANT_COLUMNS } from "../../../state/metadata/constants";
@@ -12,28 +14,57 @@ interface FileMetadataProps {
     toggleFileDetailModal: () => void;
 }
 
+interface ListItem {
+    key: string;
+    value?: any;
+}
+
+const MAC = 'Darwin';
+const WINDOWS = 'Windows_NT';
+
 const FileMetadataModal: React.FunctionComponent<FileMetadataProps> = ({ fileMetadata, toggleFileDetailModal }) => {
     if (!fileMetadata) {
         return null;
     }
-    const fileMetadataCategories: { [key: string]: JSX.Element[] } = { annotations: [], extra: [], info: [] };
+    const fileMetadataCategories: { [key: string]: ListItem[] } = { annotations: [], extra: [], info: [] };
     forOwn(fileMetadata, (value, key) => {
         // Exclude Filename since it is already shown in title
-        if (key !== 'filename') {
-            const description = (
-                <Descriptions.Item label={startCase(key)} key={key}>
-                    {value || 'None'}
-                </Descriptions.Item>
-            );
+        if (!isNil(value) && key !== 'filename' && key !== 'key') {
             if (MAIN_FILE_COLUMNS.includes(key)) {
-                fileMetadataCategories.info.push(description);
+                fileMetadataCategories.info.push({ key, value });
             } else if (UNIMPORTANT_COLUMNS.includes(key)) {
-                fileMetadataCategories.extra.push(description);
+                fileMetadataCategories.extra.push({ key, value });
             } else {
-                fileMetadataCategories.annotations.push(description);
+                fileMetadataCategories.annotations.push({ key, value });
             }
         }
     });
+    const listItemRenderer = (({ key, value }: ListItem): JSX.Element => (
+        <List.Item style={{ border: '1px solid #e8e8e8' }}>
+            <h4 className={styles.key}>{startCase(key)}</h4>
+            <span className={styles.value}>{value || 'None'}</span>
+        </List.Item>
+    ));
+    const userOS = os.type();
+    const isLocal = !!fileMetadata.localFilePath;
+    const isPublic = !!fileMetadata.publicFilePath;
+    const isArchive = !!fileMetadata.archiveFilePath;
+    const download = (filePath: string | number): void => {
+        const path = filePath as string;
+        let downloadPath;
+        if (userOS === WINDOWS) {
+            downloadPath = path.replace(/\//g, '\\');
+        } else if (userOS === MAC) {
+            downloadPath = path;
+        } else { // Linux
+            downloadPath = path;
+        }
+        const success = shell.showItemInFolder(downloadPath); // TODO: Test on different OS
+        if (!success) {
+            // set error!
+            console.log('error!');
+        }
+    };
     return (
         <Modal
             footer={null}
@@ -41,17 +72,49 @@ const FileMetadataModal: React.FunctionComponent<FileMetadataProps> = ({ fileMet
             closable={true}
             width="90%"
             onCancel={toggleFileDetailModal}
-            title={`File Details for ${fileMetadata.filename}`}
+            title={<h3>File Details For <strong>{fileMetadata.filename}</strong></h3>}
         >
-            <Descriptions bordered title="Info">
-                {fileMetadataCategories.info}
-            </Descriptions>
-            <Descriptions bordered className={styles.description} title="Annotations">
-                {fileMetadataCategories.annotations}
-            </Descriptions>
-            <Descriptions bordered className={styles.description} title="Extra File Metadata">
-                {fileMetadataCategories.extra}
-            </Descriptions>
+            <List
+                grid={{ gutter: 4, xs: 3 }}
+                dataSource={fileMetadataCategories.info}
+                renderItem={listItemRenderer}
+            />
+            <Divider>Annotations</Divider>
+            <List
+                grid={{ gutter: 4, xs: 3 }}
+                dataSource={fileMetadataCategories.annotations}
+                renderItem={listItemRenderer}
+            />
+            <Divider>Extra File Metadata</Divider>
+            <List
+                grid={{ gutter: 4, xs: 3 }}
+                dataSource={fileMetadataCategories.extra}
+                renderItem={listItemRenderer}
+            />
+            {(isArchive && !isPublic && !isLocal) && (
+                <Alert type="warning" message="File is only available for download from the archive" />
+            )}
+            {(!isArchive && !isPublic && !isLocal) && (
+                <Alert type="error" message="File doesn't appear to be available for download from anywhere" />
+            )}
+            <Row>
+                {isPublic && (
+                    <Button
+                        type="primary"
+                        className={styles.downloadButton}
+                        onClick={() => download(fileMetadata.publicFilePath)}
+                    >Browse To Public File
+                    </Button>
+                )}
+                {isLocal && (
+                    <Button
+                        type="primary"
+                        className={styles.downloadButton}
+                        onClick={() => download(fileMetadata.localFilePath)}
+                    >Browse To Local File
+                    </Button>
+                )}
+            </Row>
         </Modal>
     );
 }
