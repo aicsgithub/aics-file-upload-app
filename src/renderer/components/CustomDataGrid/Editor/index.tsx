67696d9901bea64d660acabc492e49784e0ada60
lisajoy <lisah@alleninstitute.org>
2020-02-03 16:13:11 -0800

    add search

--- src/renderer/components/CustomDataGrid/Editor/index.tsx
+++ src/renderer/components/CustomDataGrid/Editor/index.tsx
@@ -1,5 +1,6 @@
 import { DatePicker, Input, InputNumber, Select } from "antd";
 import Logger from "js-logger";
+import { noop } from "lodash";
 import * as moment from "moment";
 import * as React from "react";
 import { editors } from "react-data-grid";
@@ -23,15 +24,28 @@ interface EditorProps extends AdazzleReactDataGrid.EditorBaseProps {
     width?: string;
 }
 
+interface EditorState {
+    searchValue?: string;
+    searchResults: string[];
+}
+
 /*
     This is the editor for the UploadJobGrid, the purpose of this is to dynamically determine the editor based on
     which `type` the Editor is supplied and use that to render an appropriate form.
     Note that the field `input` and the methods `getValue` & `getInputNode` are required and used by the React-Data-Grid
     additionally, the element you return must contain an Input element
  */
-class Editor extends editors.EditorBase<EditorProps, {}> {
+class Editor extends editors.EditorBase<EditorProps, EditorState> {
     // This ref is here so that the DataGrid doesn't throw a fit, normally it would use this to .focus() the input
     public input = React.createRef<HTMLDivElement>();
+
+    constructor(props: EditorProps) {
+        super(props);
+        this.state = {
+            searchResults: [],
+            searchValue: undefined,
+        };
+    }
 
     public render() {
         const { column: { allowMultipleValues, dropdownValues, type }, value } = this.props;
@@ -107,6 +121,11 @@ class Editor extends editors.EditorBase<EditorProps, {}> {
                 );
                 break;
             case ColumnType.LOOKUP:
+                const useSearch = dropdownValues && dropdownValues.length > 20;
+                const options: string[] = useSearch ?
+                    this.state.searchResults : (dropdownValues || []);
+                console.log("useSearch", useSearch)
+                console.log("value", this.state.searchValue || value)
                 input = (
                     <Select
                         allowClear={true}
@@ -114,14 +133,16 @@ class Editor extends editors.EditorBase<EditorProps, {}> {
                         defaultOpen={true}
                         loading={!dropdownValues || !dropdownValues.length}
                         mode={allowMultipleValues ? "multiple" : "default"}
+                        notFoundContent={useSearch ? "Try searching" : "No Results"}
                         onChange={this.handleOnChange}
                         onBlur={this.props.onCommit}
+                        onSearch={useSearch ? this.onSearch : noop}
                         placeholder="Column Values"
                         showSearch={true}
                         style={{ width: "100%" }}
-                        value={value}
+                        value={this.state.searchValue || value}
                     >
-                        {dropdownValues && dropdownValues.map((dropdownValue: string) => (
+                        {options.map((dropdownValue: string) => (
                             <Option key={dropdownValue} value={dropdownValue}>{dropdownValue}</Option>
                         ))}
                     </Select>
@@ -153,11 +174,23 @@ class Editor extends editors.EditorBase<EditorProps, {}> {
     }
 
     private handleOnChange = (value: any) => {
+        console.log("on change")
         const { column: { key, onChange }, rowData } = this.props;
+        this.setState({searchResults: [], searchValue: undefined});
 
         if (onChange) {
             onChange(value, key, rowData);
         }
+    }
+
+    private onSearch = (value: string) => {
+        console.log("on search", value);
+        const allValues = this.props.column.dropdownValues || [];
+        console.log(allValues.filter((v: string) => v.includes(value)));
+        this.setState({
+            searchResults: allValues.filter((v: string) => v.includes(value)),
+            searchValue: value,
+        });
     }
 }
 
