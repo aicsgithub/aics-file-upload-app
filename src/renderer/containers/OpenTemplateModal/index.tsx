e6f57c716a33ed98ed625fb51e2b7d2ec27aea6f
Lisa Harrylock <lisah@alleninstitute.org>
2020-02-12 16:01:09 -0800

    wip

--- src/renderer/containers/OpenTemplateModal/index.tsx
+++ src/renderer/containers/OpenTemplateModal/index.tsx
@@ -7,9 +7,6 @@ import { ActionCreator } from "redux";
 
 import { OPEN_OPEN_TEMPLATE_MODAL } from "../../../shared/constants";
 import TemplateSearch from "../../components/TemplateSearch";
-import { requestTemplates } from "../../state/metadata/actions";
-import { getTemplates } from "../../state/metadata/selectors";
-import { GetTemplatesAction } from "../../state/metadata/types";
 import { closeOpenTemplateModal, openOpenTemplateModal, openTemplateEditor } from "../../state/selection/actions";
 import { getOpenTemplateModalVisible } from "../../state/selection/selectors";
 import {
@@ -21,7 +18,6 @@ import {
 import {
     State,
 } from "../../state/types";
-import { LabkeyTemplate } from "../../util/labkey-client/types";
 
 const styles = require("./styles.pcss");
 
@@ -30,17 +26,10 @@ interface OpenTemplateModalProps {
     close: ActionCreator<CloseOpenTemplateModalAction>;
     openOpenTemplateModal: ActionCreator<OpenOpenTemplateModalAction>;
     openTemplateEditor: (templateId?: number) => OpenTemplateEditorAction;
-    requestTemplates: ActionCreator<GetTemplatesAction>;
-    templates: LabkeyTemplate[];
     visible: boolean;
 }
 
 class OpenTemplateModal extends React.Component<OpenTemplateModalProps, {}> {
-    constructor(props: OpenTemplateModalProps) {
-        super(props);
-        this.state = {};
-    }
-
     public componentDidMount(): void {
         ipcRenderer.on(OPEN_OPEN_TEMPLATE_MODAL, this.props.openOpenTemplateModal);
     }
@@ -49,14 +38,8 @@ class OpenTemplateModal extends React.Component<OpenTemplateModalProps, {}> {
         ipcRenderer.removeListener(OPEN_OPEN_TEMPLATE_MODAL, this.props.openOpenTemplateModal);
     }
 
-    public componentDidUpdate(prevProps: OpenTemplateModalProps): void {
-        if (this.props.visible && this.props.visible !== prevProps.visible) {
-            this.props.requestTemplates();
-        }
-    }
-
     public render() {
-        const { className, close, templates, visible } = this.props;
+        const { className, close, visible } = this.props;
         return (
             <Modal
                 className={classNames(styles.container, className)}
@@ -64,7 +47,7 @@ class OpenTemplateModal extends React.Component<OpenTemplateModalProps, {}> {
                 title="Open Template"
                 visible={visible}
             >
-                <TemplateSearch className={styles.search} templates={templates} onSelect={this.openTemplateEditor}/>
+                <TemplateSearch className={styles.search} onSelect={this.openTemplateEditor}/>
             </Modal>
         );
     }
@@ -77,7 +60,6 @@ class OpenTemplateModal extends React.Component<OpenTemplateModalProps, {}> {
 
 function mapStateToProps(state: State) {
     return {
-        templates: getTemplates(state),
         visible: getOpenTemplateModalVisible(state),
     };
 }
@@ -86,7 +68,6 @@ const dispatchToPropsMap = {
     close: closeOpenTemplateModal,
     openOpenTemplateModal,
     openTemplateEditor,
-    requestTemplates,
 };
 
 export default connect(mapStateToProps, dispatchToPropsMap)(OpenTemplateModal);
