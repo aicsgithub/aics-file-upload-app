092293ce13e4d1bad74e4b0fee16b1f555d3c604
lisajoy <lisah@alleninstitute.org>
2020-06-01 16:20:43 -0700

    upgrade node types and remove import change for autoupdater

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import { app, BrowserWindow, Event, ipcMain } from "electron";
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
 } from "electron-devtools-installer";
+import { autoUpdater } from "electron-updater";
 
 import {
   LIMS_PROTOCOL,
@@ -14,9 +15,6 @@ import {
 } from "../shared/constants";
 
 import { setMenu } from "./menu";
-
-// https://github.com/electron/electron/issues/10167
-const { autoUpdater } = require("electron-updater");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
