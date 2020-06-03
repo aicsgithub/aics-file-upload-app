70434049d76eb29beeaefd91813e161f191d716a
Matte Bailey <matteb@alleninstitute.org>
2020-06-02 17:07:46 -0700

    Make notes field not editable through normal editor

--- src/renderer/components/CustomDataGrid/index.tsx
+++ src/renderer/components/CustomDataGrid/index.tsx
@@ -306,7 +306,6 @@ class CustomDataGrid extends React.Component<Props, CustomDataState> {
       },
       ...innerColumns,
       {
-        editable: true,
         formatter: ({ row }: FormatterProps<UploadJobTableRow>) => (
           <div
             className={classNames(
