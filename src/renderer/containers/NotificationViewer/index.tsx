361f825e2e26fff743db75bc001419e5ec4ad383
Matte Bailey <matteb@alleninstitute.org>
2020-11-06 14:41:58 -0800

    Add initial elements and styling for notification settings

--- src/renderer/containers/NotificationViewer/index.tsx
+++ src/renderer/containers/NotificationViewer/index.tsx
@@ -1,8 +1,8 @@
-import { Icon, Modal } from "antd";
+import { Button, Icon, Modal, Switch } from "antd";
 import * as classNames from "classnames";
 import * as moment from "moment";
-import { useState } from "react";
 import * as React from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { getEventsByNewest } from "../../state/feedback/selectors";
@@ -33,6 +33,10 @@ const iconPropsLookup = {
   },
 };
 
+function getIcon(type: AlertType) {
+  return <Icon theme="filled" {...iconPropsLookup[type]} />;
+}
+
 function formatDate(date: Date): string {
   return moment(date).format("MM/DD/YYYY [at] HH:mm A");
 }
@@ -40,19 +44,94 @@ function formatDate(date: Date): string {
 export default function NotificationViewer() {
   const events = useSelector(getEventsByNewest);
   const [showEvents, setShowEvents] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(true);
+  const [showErrors, setShowErrors] = useState(true);
+  const [showWarnings, setShowWarnings] = useState(true);
+  const [showInfo, setShowInfo] = useState(true);
+  const [showDraftSaved, setShowDraftSaved] = useState(false);
+
+  const modalHeader = (
+    <div className={styles.modalHeader}>
+      Notifications
+      <Icon
+        type="setting"
+        theme="filled"
+        className={styles.settingsIcon}
+        onClick={() => setShowSettings(true)}
+      />
+    </div>
+  );
 
   const eventList = events.map((event) => (
     <div
       key={event.date.toISOString()}
       className={styles.notificationContainer}
     >
-      <div className={styles.iconContainer}>
-        <Icon theme="filled" {...iconPropsLookup[event.type]} />
-      </div>
+      <div className={styles.iconContainer}>{getIcon(event.type)}</div>
       <div className={styles.message}>{event.message}</div>
       <div className={styles.timestamp}>{formatDate(event.date)}</div>
     </div>
   ));
+
+  const settingsItems = [
+    {
+      type: AlertType.SUCCESS,
+      label: "Success",
+      checked: showSuccess,
+      setter: setShowSuccess,
+    },
+    {
+      type: AlertType.ERROR,
+      label: "Error",
+      checked: showErrors,
+      setter: setShowErrors,
+    },
+    {
+      type: AlertType.WARN,
+      label: "Warning",
+      checked: showWarnings,
+      setter: setShowWarnings,
+    },
+    {
+      type: AlertType.INFO,
+      label: "Info",
+      checked: showInfo,
+      setter: setShowInfo,
+    },
+    {
+      type: AlertType.DRAFT_SAVED,
+      label: "Draft Saved",
+      checked: showDraftSaved,
+      setter: setShowDraftSaved,
+    },
+  ];
+
+  const settingsPage = (
+    <>
+      <div className={styles.settingsTitle}>Notification Settings</div>
+      <div className={styles.toggleLabel}>Show in Notification Center</div>
+      {settingsItems.map((item) => (
+        <div key={item.type} className={styles.settingsContainer}>
+          <div className={styles.notificationType}>
+            {getIcon(item.type)} {item.label}
+          </div>
+          <div className={styles.toggle}>
+            <Switch
+              checked={item.checked}
+              onChange={(checked) => item.setter(checked)}
+            />
+          </div>
+        </div>
+      ))}
+      <div className={styles.settingsButtons}>
+        <Button type="danger" onClick={() => setShowSettings(false)}>
+          Cancel
+        </Button>
+        <Button type="primary">Apply</Button>
+      </div>
+    </>
+  );
 
   return (
     <>
@@ -63,14 +142,15 @@ export default function NotificationViewer() {
         onClick={() => setShowEvents(true)}
       />
       <Modal
-        title="Notifications"
+        title={modalHeader}
         visible={showEvents}
         mask={false}
         footer={null}
         onCancel={() => setShowEvents(false)}
+        closable={false}
         wrapClassName="notification-modal"
       >
-        {eventList}
+        {showSettings ? settingsPage : eventList}
       </Modal>
     </>
   );
